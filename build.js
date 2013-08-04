bb58ff068a7f9a57edcf3e96ae686ad8b28c6237
Makis Tracend <makis.tracend@gmail.com>
2013-08-03 23:58:33 -0700

    upgrading uglify

--- build.js
+++ build.js
@@ -1,117 +1,116 @@
 // settings
 var FILE_ENCODING = 'utf-8',
-    EOL = '\n';
- 
+	EOL = '\n';
+
 // Dependencies
 var _cli = require('commander'),
-	_uglify = require("uglify-js2"), 
-    _jshint = require('jshint'),
-	_handlebars = require('hbs'), 
-    _fs = require('fs');
- 
- 
- // Logic 
- // - concatinate all files
+	_uglify = require("uglify-js"),
+	_jshint = require('jshint'),
+	_handlebars = require('hbs'),
+	_fs = require('fs');
+
+
+// Logic
+// - concatinate all files
 concat({
-    src : [
-		'lib/helpers.js', 
+	src : [
+		'lib/helpers.js',
 		'lib/construct.js',
 		'lib/config.js',
 		'lib/models.js',
 		'lib/views.js',
 		'lib/controllers.js'
-    ],
-    dest : 'build/construct.js'
+	],
+	dest : 'build/construct.js'
 });
 
 
 // - Validate js
 lint('build/construct.js', function(){
-    
+
 	// - Create / save minified file
 	uglify('build/construct.js', 'build/construct-min.js');
 
 });
 
 
-
 //
 // Methods
 function concat(opts) {
-    var fileList = opts.src;
-    var distPath = opts.dest;
-	
+	var fileList = opts.src;
+	var distPath = opts.dest;
+
 	var lib = fileList.map(function(filePath){
-            return _fs.readFileSync(filePath, FILE_ENCODING);
-        });
-		
+			return _fs.readFileSync(filePath, FILE_ENCODING);
+		});
+
 	var wrapper = _fs.readFileSync('lib/init.js', FILE_ENCODING);
-	
+
 	var template = _handlebars.compile( wrapper );
-	 
+
 	//reuse package.json data and add build date
 	var data = JSON.parse( _fs.readFileSync('package.json', FILE_ENCODING) );
 	data.lib = lib.join(EOL);
 	data.build_date = (new Date()).toUTCString();
-	
+
 	// Save uncompressed file
-    _fs.writeFileSync(distPath, template(data), FILE_ENCODING);
-    console.log(' '+ distPath +' built.');
-	
+	_fs.writeFileSync(distPath, template(data), FILE_ENCODING);
+	console.log(' '+ distPath +' built.');
+
 }
 
 
 function uglify(srcPath, distPath) {
 	/*
-    var
-      jsp = uglyfyJS.parser,
-      pro = uglyfyJS.uglify,
-      ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
- 
-    ast = pro.ast_mangle(ast);
-    ast = pro.ast_squeeze(ast);
- 	*/
-	
+	var
+	  jsp = uglyfyJS.parser,
+	  pro = uglyfyJS.uglify,
+	  ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
+
+	ast = pro.ast_mangle(ast);
+	ast = pro.ast_squeeze(ast);
+	*/
+
 	var result = _uglify.minify(srcPath, { compressor: {
 		comments : true
 	} });
-	
-    _fs.writeFileSync(distPath, result.code, FILE_ENCODING);
-    console.log(' '+ distPath +' built.');
-}
- 
-function lint(path, callback) {
-    var buf = _fs.readFileSync(path, 'utf-8');
-    // remove Byte Order Mark
-    buf = buf.replace(/^\uFEFF/, '');
- 
-    _jshint.JSHINT(buf);
- 
-    var nErrors = _jshint.JSHINT.errors.length;
- 
-    if (nErrors) {
-		// ruff output of errors (for now)
- 		console.log(_jshint.JSHINT.errors);
-        console.log(' Found %j lint errors on %s, do you want to continue?', nErrors, path);
-		
-        _cli.choose(['no', 'yes'], function(i){
-            if (i) {
-                process.stdin.destroy();
-                if(callback) callback();
-            } else {
-                process.exit(0);
-            }
-        });
-    } else if (callback) {
-        callback();
-    }
+
+	_fs.writeFileSync(distPath, result.code, FILE_ENCODING);
+	console.log(' '+ distPath +' built.');
 }
 
- 
+function lint(path, callback) {
+	var buf = _fs.readFileSync(path, 'utf-8');
+	// remove Byte Order Mark
+	buf = buf.replace(/^\uFEFF/, '');
+
+	_jshint.JSHINT(buf);
+
+	var nErrors = _jshint.JSHINT.errors.length;
+
+	if (nErrors) {
+		// ruff output of errors (for now)
+		console.log(_jshint.JSHINT.errors);
+		console.log(' Found %j lint errors on %s, do you want to continue?', nErrors, path);
+
+		_cli.choose(['no', 'yes'], function(i){
+			if (i) {
+				process.stdin.destroy();
+				if(callback) callback();
+			} else {
+				process.exit(0);
+			}
+		});
+	} else if (callback) {
+		callback();
+	}
+}
+
+
 // will generate a CSV if package.json contains multiple licenses
 _handlebars.registerHelper('license', function(items){
-    items = items.map(function(val){
-        return val.type;
-    });
-    return items.join(', ');
+	items = items.map(function(val){
+		return val.type;
+	});
+	return items.join(', ');
 });
