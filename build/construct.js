49bc6133b2f2273124c75d214d504ff7eaf14fd4
Makis Tracend <makis.tracend@gmail.com>
2013-08-04 05:38:51 -0700

    check main dependencies on initialization

--- build/construct.js
+++ build/construct.js
@@ -72,15 +72,20 @@ var locale = {
 
 construct = function( options, callback ){
 
+	// prerequisites
+	if( typeof Backbone == "undefined" ) return construct.log("error", "no-backbone");
+	if( typeof jQuery == "undefined" ) return construct.log("error", "no-jquery");
+	if( typeof jQuery.fn.three == "undefined" ) return construct.log("error", "no-jquery-three");
+
 	// extend default config with supplied config
 	//if( options.deps ) construct.config = $.extend( true, options.deps, construct.config);
-	if( options.deps ) Object.extend(construct.config, options.deps);
+	//if( options.deps ) Object.extend(construct.config, options.deps);
 
 	if( callback ) construct.callback = callback;
 
 	// execute any config options passed in the init()
 	construct.promise.resolve();
-	// set the initi method
+	// set the init method
 	//construct.config.init = construct.init;
 
 	//require.config( construct.config );
